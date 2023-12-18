7b46f89a1f4241d9460a12e373062e1a9c1aa3a7
JustLobo1121 <lobo1121personal@gmail.com>
2023-12-18 01:12:32 -0300

    update footer

--- src/components/Bajada.jsx
+++ src/components/Bajada.jsx
@@ -3,7 +3,15 @@ import '../Bajada.css'
 const Bajada = () => {
     return (
         <footer className='footer'>
-            <h4>manuel.eduardo.hernandez255@gmail.com 👍</h4>
+            <h4>manuel.eduardo.hernandez255@gmail.com
+                <a href='https://github.com/JustLobo1121' target="_blank" rel="noopener noreferrer">
+                    github
+                </a>
+                -
+                <a href="https://www.linkedin.com/in/manuel-hernández-5ab6bb267/" target="_blank" rel="noopener noreferrer">
+                    Linkedin
+                </a>
+            </h4>
         </footer>
     )
 }
